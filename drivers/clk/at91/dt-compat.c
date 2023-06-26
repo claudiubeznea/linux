7c7ccd241b49146642feee1a6cf593197ae0ad78
Claudiu Beznea <claudiu.beznea@microchip.com>
2023-06-26 14:11:40 +0300

    clk: at91: dt-compat: switch to parent_hw and parent_data
    Switch old dt-compat clocks to use parent_hw and parent_data. Having
    parent_hw instead of parent names improves to clock registration
    speed and re-parenting.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- drivers/clk/at91/dt-compat.c
+++ drivers/clk/at91/dt-compat.c
@@ -32,6 +32,7 @@ static void __init of_sama5d2_clk_audio_pll_frac_setup(struct device_node *np)
 	struct clk_hw *hw;
 	const char *name = np->name;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 	struct device_node *parent_np;
 
@@ -42,8 +43,9 @@ static void __init of_sama5d2_clk_audio_pll_frac_setup(struct device_node *np)
 		return;
 
 	parent_name = of_clk_get_parent_name(np, 0);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
 
-	hw = at91_clk_register_audio_pll_frac(regmap, name, parent_name, NULL);
+	hw = at91_clk_register_audio_pll_frac(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -58,6 +60,7 @@ static void __init of_sama5d2_clk_audio_pll_pad_setup(struct device_node *np)
 	struct clk_hw *hw;
 	const char *name = np->name;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 	struct device_node *parent_np;
 
@@ -68,8 +71,9 @@ static void __init of_sama5d2_clk_audio_pll_pad_setup(struct device_node *np)
 		return;
 
 	parent_name = of_clk_get_parent_name(np, 0);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
 
-	hw = at91_clk_register_audio_pll_pad(regmap, name, parent_name, NULL);
+	hw = at91_clk_register_audio_pll_pad(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -84,6 +88,7 @@ static void __init of_sama5d2_clk_audio_pll_pmc_setup(struct device_node *np)
 	struct clk_hw *hw;
 	const char *name = np->name;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 	struct device_node *parent_np;
 
@@ -94,8 +99,9 @@ static void __init of_sama5d2_clk_audio_pll_pmc_setup(struct device_node *np)
 		return;
 
 	parent_name = of_clk_get_parent_name(np, 0);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
 
-	hw = at91_clk_register_audio_pll_pmc(regmap, name, parent_name, NULL);
+	hw = at91_clk_register_audio_pll_pmc(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -129,6 +135,7 @@ static void __init of_sama5d2_clk_generated_setup(struct device_node *np)
 	struct clk_hw *hw;
 	unsigned int num_parents;
 	const char *parent_names[GENERATED_SOURCE_MAX];
+	struct clk_parent_data *parent_data[GENERATED_SOURCE_MAX];
 	struct device_node *gcknp, *parent_np;
 	struct clk_range range = CLK_RANGE(0, 0);
 	struct regmap *regmap;
@@ -149,6 +156,8 @@ static void __init of_sama5d2_clk_generated_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
+	for (i = 0; i < num_parents; i++)
+		parent_data[i] = AT91_CLK_PD_NAME(parent_names[i], i);
 	for_each_child_of_node(np, gcknp) {
 		int chg_pid = INT_MIN;
 
@@ -171,7 +180,7 @@ static void __init of_sama5d2_clk_generated_setup(struct device_node *np)
 
 		hw = at91_clk_register_generated(regmap, &pmc_pcr_lock,
 						 &dt_pcr_layout, name,
-						 parent_names, NULL, NULL,
+						 NULL, parent_data, NULL,
 						 num_parents, id, &range,
 						 chg_pid);
 		if (IS_ERR(hw))
@@ -190,6 +199,7 @@ static void __init of_sama5d4_clk_h32mx_setup(struct device_node *np)
 	struct clk_hw *hw;
 	const char *name = np->name;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 	struct device_node *parent_np;
 
@@ -200,8 +210,9 @@ static void __init of_sama5d4_clk_h32mx_setup(struct device_node *np)
 		return;
 
 	parent_name = of_clk_get_parent_name(np, 0);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
 
-	hw = at91_clk_register_h32mx(regmap, name, parent_name, NULL);
+	hw = at91_clk_register_h32mx(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -219,6 +230,7 @@ static void __init of_sama5d2_clk_i2s_mux_setup(struct device_node *np)
 	struct regmap *regmap_sfr;
 	u8 bus_id;
 	const char *parent_names[2];
+	struct clk_parent_data *parent_data[2];
 	struct device_node *i2s_mux_np;
 	struct clk_hw *hw;
 	int ret;
@@ -238,8 +250,10 @@ static void __init of_sama5d2_clk_i2s_mux_setup(struct device_node *np)
 		if (ret != 2)
 			continue;
 
+		parent_data[0] = AT91_CLK_PD_NAME(parent_names[0], 0);
+		parent_data[1] = AT91_CLK_PD_NAME(parent_names[1], 1);
 		hw = at91_clk_i2s_mux_register(regmap_sfr, i2s_mux_np->name,
-					       parent_names, NULL, 2, bus_id);
+					       NULL, parent_data, 2, bus_id);
 		if (IS_ERR(hw))
 			continue;
 
@@ -255,6 +269,7 @@ static void __init of_at91rm9200_clk_main_osc_setup(struct device_node *np)
 	struct clk_hw *hw;
 	const char *name = np->name;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 	bool bypass;
 	struct device_node *parent_np;
@@ -269,7 +284,8 @@ static void __init of_at91rm9200_clk_main_osc_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91_clk_register_main_osc(regmap, name, parent_name, NULL, bypass);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91_clk_register_main_osc(regmap, name, NULL, parent_data, bypass);
 	if (IS_ERR(hw))
 		return;
 
@@ -310,6 +326,7 @@ static void __init of_at91rm9200_clk_main_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	struct regmap *regmap;
 	struct device_node *parent_np;
@@ -323,7 +340,8 @@ static void __init of_at91rm9200_clk_main_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91_clk_register_rm9200_main(regmap, name, parent_name, NULL);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91_clk_register_rm9200_main(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -336,6 +354,7 @@ static void __init of_at91sam9x5_clk_main_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_names[2];
+	struct clk_parent_data *parent_data[2];
 	unsigned int num_parents;
 	const char *name = np->name;
 	struct regmap *regmap;
@@ -354,7 +373,9 @@ static void __init of_at91sam9x5_clk_main_setup(struct device_node *np)
 
 	of_property_read_string(np, "clock-output-names", &name);
 
-	hw = at91_clk_register_sam9x5_main(regmap, name, parent_names, NULL,
+	parent_data[0] = AT91_CLK_PD_NAME(parent_names[0], 0);
+	parent_data[1] = AT91_CLK_PD_NAME(parent_names[1], 1);
+	hw = at91_clk_register_sam9x5_main(regmap, name, NULL, parent_data,
 					   num_parents);
 	if (IS_ERR(hw))
 		return;
@@ -396,6 +417,7 @@ of_at91_clk_master_setup(struct device_node *np,
 	struct clk_hw *hw;
 	unsigned int num_parents;
 	const char *parent_names[MASTER_SOURCE_MAX];
+	struct clk_parent_data *parent_data[MASTER_SOURCE_MAX];
 	const char *name = np->name;
 	struct clk_master_characteristics *characteristics;
 	struct regmap *regmap;
@@ -419,13 +441,16 @@ of_at91_clk_master_setup(struct device_node *np,
 	if (IS_ERR(regmap))
 		return;
 
+	for (unsigned int i = 0; i < MASTER_SOURCE_MAX; i++)
+		parent_data[i] = AT91_CLK_PD_NAME(parent_names[i], i);
 	hw = at91_clk_register_master_pres(regmap, "masterck_pres", num_parents,
-					   parent_names, NULL, layout,
+					   NULL, parent_data, layout,
 					   characteristics, &mck_lock);
 	if (IS_ERR(hw))
 		goto out_free_characteristics;
 
-	hw = at91_clk_register_master_div(regmap, name, "masterck_pres", NULL,
+	parent_data[0] = AT91_CLK_PD_HW(hw);
+	hw = at91_clk_register_master_div(regmap, name, NULL, parent_data,
 					  layout, characteristics,
 					  &mck_lock, CLK_SET_RATE_GATE, 0);
 	if (IS_ERR(hw))
@@ -459,6 +484,7 @@ of_at91_clk_periph_setup(struct device_node *np, u8 type)
 	u32 id;
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name;
 	struct device_node *periphclknp;
 	struct regmap *regmap;
@@ -478,6 +504,7 @@ of_at91_clk_periph_setup(struct device_node *np, u8 type)
 	if (IS_ERR(regmap))
 		return;
 
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
 	for_each_child_of_node(np, periphclknp) {
 		if (of_property_read_u32(periphclknp, "reg", &id))
 			continue;
@@ -490,7 +517,7 @@ of_at91_clk_periph_setup(struct device_node *np, u8 type)
 
 		if (type == PERIPHERAL_AT91RM9200) {
 			hw = at91_clk_register_peripheral(regmap, name,
-							  parent_name, NULL, id);
+							  NULL, parent_data, id);
 		} else {
 			struct clk_range range = CLK_RANGE(0, 0);
 			unsigned long flags = 0;
@@ -511,8 +538,8 @@ of_at91_clk_periph_setup(struct device_node *np, u8 type)
 								 &pmc_pcr_lock,
 								 &dt_pcr_layout,
 								 name,
-								 parent_name,
 								 NULL,
+								 parent_data,
 								 id, &range,
 								 INT_MIN,
 								 flags);
@@ -643,6 +670,7 @@ of_at91_clk_pll_setup(struct device_node *np,
 	struct clk_hw *hw;
 	struct regmap *regmap;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	struct device_node *parent_np;
 	struct clk_pll_characteristics *characteristics;
@@ -664,7 +692,8 @@ of_at91_clk_pll_setup(struct device_node *np,
 	if (!characteristics)
 		return;
 
-	hw = at91_clk_register_pll(regmap, name, parent_name, NULL, id, layout,
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91_clk_register_pll(regmap, name, NULL, parent_data, id, layout,
 				   characteristics);
 	if (IS_ERR(hw))
 		goto out_free_characteristics;
@@ -709,6 +738,7 @@ of_at91sam9x5_clk_plldiv_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	struct regmap *regmap;
 	struct device_node *parent_np;
@@ -723,7 +753,8 @@ of_at91sam9x5_clk_plldiv_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91_clk_register_plldiv(regmap, name, parent_name, NULL);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91_clk_register_plldiv(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -742,6 +773,7 @@ of_at91_clk_prog_setup(struct device_node *np,
 	struct clk_hw *hw;
 	unsigned int num_parents;
 	const char *parent_names[PROG_SOURCE_MAX];
+	struct clk_parent_data *parent_data[PROG_SOURCE_MAX];
 	const char *name;
 	struct device_node *progclknp, *parent_np;
 	struct regmap *regmap;
@@ -762,6 +794,8 @@ of_at91_clk_prog_setup(struct device_node *np,
 	if (IS_ERR(regmap))
 		return;
 
+	for (unsigned int i = 0; i < PROG_SOURCE_MAX; i++)
+		parent_data[i] = AT91_CLK_PD_NAME(parent_names[i], i);
 	for_each_child_of_node(np, progclknp) {
 		if (of_property_read_u32(progclknp, "reg", &id))
 			continue;
@@ -770,7 +804,7 @@ of_at91_clk_prog_setup(struct device_node *np,
 			name = progclknp->name;
 
 		hw = at91_clk_register_programmable(regmap, name,
-						    parent_names, NULL, num_parents,
+						    NULL, parent_data, num_parents,
 						    id, layout, mux_table);
 		if (IS_ERR(hw))
 			continue;
@@ -804,6 +838,7 @@ static void __init of_at91sam9260_clk_slow_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_names[2];
+	struct clk_parent_data *parent_data[2];
 	unsigned int num_parents;
 	const char *name = np->name;
 	struct regmap *regmap;
@@ -822,7 +857,9 @@ static void __init of_at91sam9260_clk_slow_setup(struct device_node *np)
 
 	of_property_read_string(np, "clock-output-names", &name);
 
-	hw = at91_clk_register_sam9260_slow(regmap, name, parent_names, NULL,
+	parent_data[0] = AT91_CLK_PD_NAME(parent_names[0], 0);
+	parent_data[1] = AT91_CLK_PD_NAME(parent_names[1], 1);
+	hw = at91_clk_register_sam9260_slow(regmap, name, NULL, parent_data,
 					    num_parents);
 	if (IS_ERR(hw))
 		return;
@@ -840,6 +877,7 @@ static void __init of_at91sam9x5_clk_smd_setup(struct device_node *np)
 	struct clk_hw *hw;
 	unsigned int num_parents;
 	const char *parent_names[SMD_SOURCE_MAX];
+	struct clk_parent_data *parent_data[SMD_SOURCE_MAX];
 	const char *name = np->name;
 	struct regmap *regmap;
 	struct device_node *parent_np;
@@ -858,7 +896,9 @@ static void __init of_at91sam9x5_clk_smd_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91sam9x5_clk_register_smd(regmap, name, parent_names, NULL,
+	for (unsigned int i = 0; i < SMD_SOURCE_MAX; i++)
+		parent_data[i] = AT91_CLK_PD_NAME(parent_names[i], i);
+	hw = at91sam9x5_clk_register_smd(regmap, name, NULL, parent_data,
 					 num_parents);
 	if (IS_ERR(hw))
 		return;
@@ -877,6 +917,7 @@ static void __init of_at91rm9200_clk_sys_setup(struct device_node *np)
 	const char *name;
 	struct device_node *sysclknp, *parent_np;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	struct regmap *regmap;
 
 	num = of_get_child_count(np);
@@ -908,7 +949,8 @@ static void __init of_at91rm9200_clk_sys_setup(struct device_node *np)
 		if (!strcmp(sysclknp->name, "ddrck"))
 			flags = CLK_IS_CRITICAL;
 
-		hw = at91_clk_register_system(regmap, name, parent_name, NULL,
+		parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+		hw = at91_clk_register_system(regmap, name, NULL, parent_data,
 					      id, flags);
 		if (IS_ERR(hw))
 			continue;
@@ -927,6 +969,7 @@ static void __init of_at91sam9x5_clk_usb_setup(struct device_node *np)
 	struct clk_hw *hw;
 	unsigned int num_parents;
 	const char *parent_names[USB_SOURCE_MAX];
+	struct clk_parent_data *parent_data[USB_SOURCE_MAX];
 	const char *name = np->name;
 	struct regmap *regmap;
 	struct device_node *parent_np;
@@ -945,7 +988,9 @@ static void __init of_at91sam9x5_clk_usb_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91sam9x5_clk_register_usb(regmap, name, parent_names, NULL,
+	for (unsigned int i = 0; i < USB_SOURCE_MAX; i++)
+		parent_data[i] = AT91_CLK_PD_NAME(parent_names[i], i);
+	hw = at91sam9x5_clk_register_usb(regmap, name, NULL, parent_data,
 					 num_parents);
 	if (IS_ERR(hw))
 		return;
@@ -959,6 +1004,7 @@ static void __init of_at91sam9n12_clk_usb_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	struct regmap *regmap;
 	struct device_node *parent_np;
@@ -975,7 +1021,8 @@ static void __init of_at91sam9n12_clk_usb_setup(struct device_node *np)
 	if (IS_ERR(regmap))
 		return;
 
-	hw = at91sam9n12_clk_register_usb(regmap, name, parent_name, NULL);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91sam9n12_clk_register_usb(regmap, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
@@ -988,6 +1035,7 @@ static void __init of_at91rm9200_clk_usb_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	u32 divisors[4] = {0, 0, 0, 0};
 	struct regmap *regmap;
@@ -1008,7 +1056,8 @@ static void __init of_at91rm9200_clk_usb_setup(struct device_node *np)
 	of_node_put(parent_np);
 	if (IS_ERR(regmap))
 		return;
-	hw = at91rm9200_clk_register_usb(regmap, name, parent_name, NULL, divisors);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91rm9200_clk_register_usb(regmap, name, NULL, parent_data, divisors);
 	if (IS_ERR(hw))
 		return;
 
@@ -1023,6 +1072,7 @@ static void __init of_at91sam9x5_clk_utmi_setup(struct device_node *np)
 {
 	struct clk_hw *hw;
 	const char *parent_name;
+	struct clk_parent_data *parent_data;
 	const char *name = np->name;
 	struct regmap *regmap_pmc, *regmap_sfr;
 	struct device_node *parent_np;
@@ -1055,7 +1105,8 @@ static void __init of_at91sam9x5_clk_utmi_setup(struct device_node *np)
 			regmap_sfr = NULL;
 	}
 
-	hw = at91_clk_register_utmi(regmap_pmc, regmap_sfr, name, parent_name, NULL);
+	parent_data = AT91_CLK_PD_NAME(parent_name, 0);
+	hw = at91_clk_register_utmi(regmap_pmc, regmap_sfr, name, NULL, parent_data);
 	if (IS_ERR(hw))
 		return;
 
